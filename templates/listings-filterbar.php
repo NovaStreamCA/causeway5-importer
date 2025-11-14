9c35eb5b8ede50846e9b884432009429e8b40ab0
GeorgeDylan <claymoarplays@gmail.com>
2025-11-14 19:17:30 +0000

    style filter

--- templates/listings-filterbar.php
+++ templates/listings-filterbar.php
@@ -21,8 +21,15 @@ $cats = get_terms([
 
 <div class="causeway-filterbar" role="region" aria-label="Listings filters">
     <div class="filter-row">
-        <label for="causeway-filter-search" class="screen-reader-text"><?php esc_html_e('Search by title', 'causeway'); ?></label>
-        <input id="causeway-filter-search" type="search" placeholder="<?php echo esc_attr__('Search by title…', 'causeway'); ?>" data-role="search" />
+        <label for="causeway-filter-search" class="screen-reader-text"><?php esc_html_e('Search listings', 'causeway'); ?></label>
+        <div class="cs-input-group">
+            <div class="input-group-text">
+                <svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.7549 14.255H14.9649L14.6849 13.985C15.6649 12.845 16.2549 11.365 16.2549 9.755C16.2549 6.165 13.3449 3.255 9.75488 3.255C6.16488 3.255 3.25488 6.165 3.25488 9.755C3.25488 13.345 6.16488 16.255 9.75488 16.255C11.3649 16.255 12.8449 15.665 13.9849 14.685L14.2549 14.965V15.755L19.2549 20.745L20.7449 19.255L15.7549 14.255ZM9.75488 14.255C7.26488 14.255 5.25488 12.245 5.25488 9.755C5.25488 7.26501 7.26488 5.255 9.75488 5.255C12.2449 5.255 14.2549 7.26501 14.2549 9.755C14.2549 12.245 12.2449 14.255 9.75488 14.255Z"/>
+                </svg>
+            </div>
+            <input id="causeway-filter-search" type="search" placeholder="<?php echo esc_attr__('Search listings…', 'causeway'); ?>" data-role="search" />
+        </div>
 
         <label for="causeway-filter-type" class="screen-reader-text"><?php esc_html_e('Filter by type', 'causeway'); ?></label>
         <select id="causeway-filter-type" data-role="select-type">
